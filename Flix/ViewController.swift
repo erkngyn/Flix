a0a7fcdfab261f2ac2ceba0d9299c9b21c7ed1d0
Erik Nguyen <nguyenec@uci.edu>
2020-01-21 16:01:36 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Erik Nguyen on 1/21/20.
+//  Copyright © 2020 Erik Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
